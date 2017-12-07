882d8a79ed7e935e5c4e31399782853ba7baeccb
mkuzmick <mkuzmick@gmail.com>
2017-12-07 08:57:22 -0500

    ready for g docs 20171207

--- routes/tools.js
+++ routes/tools.js
@@ -25,14 +25,15 @@ router.post('/io2s', function(req, res, next){
   for (var i = 0; i < req.body.segments.length; i++) {
     var inText = "\nSegment " + (i+1) + ". From " + req.body.segments[i].inHr + ":" + req.body.segments[i].inMin + ":" + req.body.segments[i].inSec + ":" + req.body.segments[i].inFrame
     var outText = " to " + req.body.segments[i].outHr + ":" + req.body.segments[i].outMin + ":" + req.body.segments[i].outSec + ":" + req.body.segments[i].outFrame + ".  ";
-    slackSummary += (inText + outText);
+    var camText = "Angle = " + req.body.segments[i].camAngle + "."
+    slackSummary += (inText + outText + camText);
   }
 
   console.log(process.env.SLACK_WEBHOOK_URL);
   // var thePayload = 'payload={"channel": "#ll-tests", "username": "theworkflow-bot", "text": "<@marlon>: just transcoded ' + path.basename(sourcePath) + ' and put it here: ' + destinationPath + ' .", "icon_emoji": ":desktop_computer:"}';
   // console.log(thePayload);
   // cp.spawnSync("curl", ['-X', 'POST', '--data-urlencode', thePayload, process.env.SLACK_WEBHOOK_URL]);
-  var thePayload = 'payload={"channel": "#ll-tests", "username": "theworkflow-bot", "text": "<@marlon>: just got a message from Google Sheets at ' + ts + ': ' + slackSummary + ' \n\n. . . does that seem right?", "icon_emoji": ":desktop_computer:"}';
+  var thePayload = 'payload={"channel": "#ll-tests", "username": "theworkflow-bot", "text": "<@marlon>: just got a message from Google Sheets at ' + ts + ' for shoot ' + req.body.segments[0].clipName + slackSummary + ' \n\n. . . does that seem right?", "icon_emoji": ":desktop_computer:"}';
   console.log("\n\nsending this to Slack:\n" + thePayload);
   cp.spawnSync("curl", ['-X', 'POST', '--data-urlencode', thePayload, process.env.SLACK_WEBHOOK_URL]);
 
